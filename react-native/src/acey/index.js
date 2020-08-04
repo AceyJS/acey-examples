34ef70c37e8b578978b17705fde62f49c2697509
Louis <fantasim.dev@gmail.com>
2020-08-04 16:09:08 +0200

    clean

--- react-native/src/acey/index.js
+++
@@ -1,8 +0,0 @@
-import AsyncStorage from '@react-native-community/async-storage'
-import { config } from 'acey'
-import PostCollection from './collections/post-collection'
-
-export const PostList = new PostCollection([], {connected: true, key: 'postlist'})
-
-config.setStoreEngine(AsyncStorage)
-config.done()
